83557022aeaf9e3e61916fa5b0f4ee37ddb1da01
rr3khan <65298773+rr3khan@users.noreply.github.com>
2024-08-08 00:49:34 -0400

    Add ability to Read file

--- clap_minigrep/src/main.rs
+++ clap_minigrep/src/main.rs
@@ -1,14 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use clap::Parser;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None )]
+
+struct Args {
+    #[arg(short, long)]
+    query: String,
+
+    #[arg(short, long)]
+    file_path: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn main() {
+    let args = Args::parse();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    println!("Hello {}!", args.query);
+    println!("Hello {}!", args.file_path);
+
+    let file = args.file_path;
+    println!("{}", file);
+
+    let contents = fs::read_to_string(file).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
